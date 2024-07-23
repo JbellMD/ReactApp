29f38c88150392c27cd80131b81996ae40b36d73
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-07-23 19:36:15 -0400

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -276,7 +276,7 @@ function ProductList() {
                     <a href="/https://jbellmd.github.io/ReactApp">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                     </a>
-                        <a href="/" style={{ textDecoration: 'none' }}>
+                        <a href="https://jbellmd.github.io/ReactApp" style={{ textDecoration: 'none' }}>
                             <div>
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
